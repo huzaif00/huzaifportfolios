e1e756e690a7787749cd4bd73e7a31154e6783e4
huzaif00 <mohdhuzaif2502@gmail.com>
2022-10-03 17:27:14 +0930

    update build for gh-pages

--- src/Service.js
+++ src/Service.js
@@ -144,7 +144,7 @@ function Service() {
                 <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
                     <div
                         className="bg-red-600 h-2.5 rounded-full dark:bg-red-500"
-                        style={{ width: "90%" }}
+                        style={{ width: "80%" }}
                     />
                 
                 </div>
