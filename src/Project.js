a180a1b9af00b58420605bfe36d17c1053c5f793
huzaif00 <mohdhuzaif2502@gmail.com>
2022-10-03 18:42:25 +0930

    update build for gh-pages

--- src/Project.js
+++ src/Project.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./project.css"
 import Project1 from "./img/p1.jpg"
 import Project2 from "./img/p2.jpg"
+import Project4 from "./img/p4.jpg"
 
 
 
@@ -79,6 +80,29 @@ function Project() {
                     <div className="buttons">
                         
                         <a href="https://github.com/huzaif00/Ticket-booking-System/blob/main/GUJ.CPP" className='project__btn'>View Source Code</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="col__3 ">
+                <div className="project__box pointer relative " >
+                    <div className="project__img__box pointer relative">
+                        <div className="project__box__img">
+                            <img src={Project4} alt="" className="project__img" />
+
+                        </div>
+                        <div className="mask__effect">
+                        </div>
+
+                    </div>
+                    <div className="project__meta absolute">
+                        <h5 className="project__text">Tesla Clone</h5>
+                        <h4 className="project__text">This is tesla clone build onlu using html ,css and java script.</h4>
+                       
+                    <div className="buttons">
+                        <a href="https://huzaif00.github.io/teslaclones.github.io/" className='project__btn'>View Project</a>
+                        <a href="https://github.com/huzaif00/teslaclones.github.io" className='project__btn'>View Source Code</a>
                         </div>
                     </div>
 
